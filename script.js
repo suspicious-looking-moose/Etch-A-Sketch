f89da64bd83ad7f05a6ab8a47a2fd19c27ef7dcf
suspicious-looking-moose <cp.jones117@gmail.com>
2022-01-19 16:12:40 +0000

    Woo.. finished the Main JS part at least. Trying to commit more... Is this right? Who knows.

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
+function loadGrid(width) { // Takes grid width as variable.
+    for (let j = 0; j < width; j++) {
+        const verticalContainer = document.createElement('div');
+        verticalContainer.classList.add('vertical-container');
+        verticalContainer.setAttribute('id', `vertical-container-${j + 1}`);
 
+        for (let i = 0; i < width; i++) {
+            const aSingleBox = document.createElement('div'); // Creates the single box and adds the new box class to the element
+            aSingleBox.classList.add('new'); // Adds the class of 'new' to the box created
+            aSingleBox.setAttribute('id', `row${i + 1}-column${j+1}`); // Adds a new id to the box
 
+            aSingleBox.addEventListener('mouseover', changeClass => { // Adds event listener to change the div class when moused over.
+                aSingleBox.classList.add('activated');
+            });
 
-function loadSixteenDiv() {  //CALLS THE CREATE DIV FUNCTION * 16 AND ADDS TO '.theDiv'
-    for (let i = 0; i < 2; i++) {
-
-        const bodyContain = document.querySelector('.vertical-container')
-        const container = document.createElement('div');
-        container.setAttribute('id', `container-no${i + 1}`);
-        container.classList.add('new-horiz-div'); 
-        bodyContain.appendChild(container);
-
-        /*for  (let i = 0; i < 5; i++) {
-            const myDiv = document.createElement('div');
-            myDiv.setAttribute('id', `div-no${i + 1}`);
-            myDiv.classList.add('new'); 
-            container.appendChild(myDiv);
-        }*/
-    };
-
-
+            verticalContainer.appendChild(aSingleBox); // add the box div to the horizontal container
+            const documentDiv = document.querySelector('.theDiv'); // Grabs the div from the file to add it all 
+            documentDiv.append(verticalContainer); // append the new box
+        };
+    };  
 };
 
-
-loadSixteenDiv();
-
-function loadGrid() {
-    for (let i = 0; i < 5; i++) {
-        const newContainer = document.querySelector('.vertical-container');
-        const newDiv = document.querySelector('.horiz-1');
-        newContainer.appendChild(newDiv);
-        console.log(newDiv);
-    };
-};
-
+loadGrid(64); 
 
 
 
