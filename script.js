18beef86ab2030f37879d9ab6f8283aa2f282bab
suspicious-looking-moose <cp.jones117@gmail.com>
2022-01-19 22:10:52 +0000

    Got the JS down I think

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
+// SETTING GLOBAL VARIABLES FOR THE ETCH A SKETCH
+let defaultWidth = 16;
+let currentWidth = 0;
+
+// SETTING UP RESET BUTTON
+const theResetButton = document.querySelector('.reset');
+theResetButton.addEventListener('click', resetButton);
+loadGrid(defaultWidth);
+
+// SETTING UP RESET SLIDER
+let slider = document.querySelector('#mySlider');
+const dimension = document.querySelector('#id1');
+const dimension2 = document.querySelector('#id2');
+slider.addEventListener('click', resetButton);
+dimension.innerHTML = slider.value;
+dimension2.innerHTML = slider.value;
+slider.oninput = function() {
+    dimension.innerHTML = this.value;
+    dimension2.innerHTML = this.value;
+
+}
+
+// Grabbing Colour Choice for page load:
+let paintbrush = document.querySelector('#colour-picker');
+paintbrush = paintbrush.value;
+// Setting paint picker to change on selection:
+const paintPicker = document.querySelector('#colour-picker');
+paintPicker.addEventListener('input', changeColour);
+
+// ETCH A SKETCH FUNCTIONS
+
+// Loads the grid:
 function loadGrid(width) { // Takes grid width as variable.
+
+    currentWidth = width;
+
     for (let j = 0; j < width; j++) {
         const verticalContainer = document.createElement('div');
         verticalContainer.classList.add('vertical-container');
@@ -8,9 +43,9 @@ function loadGrid(width) { // Takes grid width as variable.
             const aSingleBox = document.createElement('div'); // Creates the single box and adds the new box class to the element
             aSingleBox.classList.add('new'); // Adds the class of 'new' to the box created
             aSingleBox.setAttribute('id', `row${i + 1}-column${j+1}`); // Adds a new id to the box
-
-            aSingleBox.addEventListener('mouseover', changeClass => { // Adds event listener to change the div class when moused over.
-                aSingleBox.classList.add('activated');
+            aSingleBox.addEventListener('mouseover', change => { // Adds event listener to change the div class when moused over.
+                //aSingleBox.classList.add('activated');
+                aSingleBox.setAttribute('style', `background-color: ${paintbrush}`);
             });
 
             verticalContainer.appendChild(aSingleBox); // add the box div to the horizontal container
@@ -20,7 +55,53 @@ function loadGrid(width) { // Takes grid width as variable.
     };  
 };
 
-loadGrid(64); 
+function changeColour() {
+    let paintColour = document.querySelector('#colour-picker');
+    paintColour = paintColour.value;
 
+    const nodeOfBoxes = document.querySelectorAll('.new');
+    const arrayOfBoxes = Array.from(nodeOfBoxes);
+    arrayOfBoxes.forEach(element => {
+        console.log(element);
+        element.addEventListener('mouseover', changeAttribute => {
+            element.setAttribute('style', `background-color: ${paintColour}`);
+        });
+    });
+};
 
+function rainbowMode() {
+    let paintColour = document.querySelector('#colour-picker');
+    paintColour = paintColour.value;
+
+    const nodeOfBoxes = document.querySelectorAll('.new');
+    const arrayOfBoxes = Array.from(nodeOfBoxes);
+    arrayOfBoxes.forEach(element => {
+        console.log(element);
+        element.addEventListener('mouseover', changeAttribute => {
+            element.setAttribute('style', `background-color: ${getRandomColour()}`);
+        });
+    });
+};
+
+function getRandomColour() {
+    const colors = ['#ff0000', '#ffa500', '#ffff00', '#008000', '#0000ff', '#4b0082', '#ee82ee'];
+    randomColour = colors[Math.floor(Math.random() * colors.length)];
+    return randomColour;
+};
+
+// Clears the grid:
+function clearGrid(width) {
+    for (let x = 0; x < width; x++) {
+        const targetElement = document.querySelector(`#vertical-container-${x + 1}`);
+        targetElement.parentNode.removeChild(targetElement);
+    };
+};
+
+// Function for the reset button:
+function resetButton() {
+    let reloadwidth = document.getElementById('mySlider');
+    reloadwidth = reloadwidth.value;
+    clearGrid(currentWidth);
+    loadGrid(reloadwidth);
+};
 
